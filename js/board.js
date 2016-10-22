97c99c0acaa12b317985f9fbc1e5b76c1c78c4a7
Juan Carlos Álvarez Viedma <juanca.alvarez.viedma@gmail.com>
2016-10-22 11:38:45 +0200

    First Version

--- js/board.js
+++ js/board.js
@@ -1,8 +1,43 @@
-class Tablero {
+class Game {
 	constructor(){
-		this.celdas = [];
-		for(var i=0;i<3;i++){
-			this.celdas[i] = [0,0,0];
-		}
-	}
+		this.cells = [0,0,0,0,0,0,0,0,0];
+    }
+
+    checkWinner(player){
+        //Check if a player has a winner combination and returns the combination or false
+
+        var winCombis = [
+            [0,1,2],[3,4,5],[6,7,8], //Horizontal
+            [0,3,6],[1,4,7],[2,5,8], //Vertical
+            [0,4,8],[2,4,6] //Crossed
+        ];
+
+        for(var i=0;i<winCombis.length;i++){
+            var coincidences=0;
+            var combination = [];
+            for(var j=0;j<winCombis[i].length;j++){
+                if(this.cells[winCombis[i][j]]==player){
+                    coincidences++;
+                    combination[combination.length] = winCombis[i][j];
+                }else{
+                    break;
+                }
+            }
+            if(coincidences==3){
+                return combination;
+            };
+        }
+        return false;
+    }
+
+	movement(pos,player){
+	    //Change a cell status from 0 to 1, returns true if cell is empty.
+	    if(this.cells[pos]==0){
+            this.cells[pos]=player;
+            return true;
+        }
+        return false;
+    }
+
+
 }
---
+++ js/view.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Juanca on 20/10/2016.
+ */
